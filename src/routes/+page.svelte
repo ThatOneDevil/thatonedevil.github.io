10f54b3d7eafa68c7dd3a9c4203e9b788a60383f
ThatOneDevil <62218606+ThatOneDevil@users.noreply.github.com>
2024-07-12 16:01:23 +0100

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-5xl font-bold ">
+    Epik cool website
+  </h1>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
